17edfb8d39e6f4043f6d4161a9e12ede88a8c66e
Jon Gottfried <jonmarkgo@gmail.com>
2025-03-17 15:09:29 +0000

    updates

--- signals/handlers.py
+++ signals/handlers.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.utils.translation import ugettext_noop as _
+from condottieri_common.translation_compat import ugettext_noop as _
 
 def create_notice_types(sender, **kwargs):
     if "pinax.notifications" in settings.INSTALLED_APPS:
